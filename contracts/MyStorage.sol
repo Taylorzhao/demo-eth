eb2f3a5d2ddec8cb23b8eb8b58f5f1503213b5c2
Xin Zhao <794682349@qq.com>
2022-08-30 10:43:39 +0800

    s

--- contracts/MyStorage.sol
+++ contracts/MyStorage.sol
@@ -5,8 +5,12 @@ pragma solidity >=0.7.0 <0.9.0;
 contract MyStorage{
   uint storageData;
 
-  function  helper(uint x) external view returns (uint) {
+  function get(uint x) public pure returns (uint){
     return x * 2;
+  }
+
+  function  helper(uint x) external view returns (uint) {
+    return get(x);
   }
 
 }
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Token from './artifacts/contracts/Token.sol/Token.json'
 import MyStorage from './artifacts/contracts/MyStorage.sol/MyStorage.json'
 
 const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const storageToken = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+const storageToken = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 
 function  App()  {
